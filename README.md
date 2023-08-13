bc391033d134519ba69a7c9cfc425ec2cabe51bf
Ahrimdon <mrcodblackops10@gmail.com>
2023-08-13 16:46:26 -0400

    Initial commit

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ If you don't have `git` installed on your machine, follow these steps:
 
 1. Visit the [Git download page](https://git-scm.com/downloads).
 2. Download the appropriate version for your operating system.
-3. Run the installer. 
+3. Run the installer.
    - During the installation, you will be asked if you want to add Git to your system's PATH environment variable. Ensure you choose the option to do so. This allows you to use Git from the command line without specifying its full path.
 4. After installation, open a terminal or command prompt and type `git --version` to ensure Git is correctly installed.
 
@@ -27,9 +27,19 @@ If you don't have `git` installed on your machine, follow these steps:
      git clone https://github.com/YourUsername/GameClientRepo.git
      ```
 ### Setting Up
-
-1. 
-```
 1. Move the `xlabs` folder to your local app data directory.
 
 > **Note**: Ensure the game client points to the correct local app data directory for proper functionality.
+
+[![patreon](https://img.shields.io/badge/patreon-support-blue.svg?logo=patreon)](https://www.patreon.com/xlabsproject)
+[![discord](https://img.shields.io/endpoint?url=https://momo5502.com/iw4x/members-badge.php)](https://discord.gg/sKeVmR3)
+
+<p align="center">
+  <img src="assets/github/banner-s1x.png?raw=true" />
+</p>
+
+<br/>
+
+<p align="center">
+  <img alig src="assets/github/banner-iw6x.png?raw=true"/>
+</p>
---
+++ localappdata/xlabs/data/launcher-ui/js/main.js
@@ -0,0 +1,132 @@
+window.addEventListener("load", initialize);
+window.channel = window.executeCommand("get-channel");
+
+function sleep(milliseconds) {
+    return new Promise(resolve => {
+        setTimeout(resolve, milliseconds);
+    });
+}
+
+function makeSleep(milliseconds) {
+    return () => sleep(milliseconds);
+}
+
+function waitForAllImages() {
+    return new Promise(resolve => {
+        function waitForAllImagesInternal() {
+            const images = document.querySelectorAll('img');
+
+            for (var i = 0; i < images.length; ++i) {
+                if (!images[i].complete) {
+                    window.requestAnimationFrame(waitForAllImagesInternal);
+                    return;
+                }
+            }
+
+            resolve();
+        }
+
+        waitForAllImagesInternal();
+    });
+}
+
+function addStyleElement(css) {
+    var head = document.head || document.getElementsByTagName('head')[0],
+        style = document.createElement('style');
+
+    head.appendChild(style);
+
+    style.type = 'text/css';
+    if (style.styleSheet) {
+        // This is required for IE8 and below.
+        style.styleSheet.cssText = css;
+    } else {
+        style.appendChild(document.createTextNode(css));
+    }
+}
+
+function getOtherChannel(channel) {
+    if (channel == "main") {
+        return "dev";
+    }
+    return "main";
+}
+
+function adjustChannelElements() {
+    window.channel.then(channel => {
+        addStyleElement(`.channel-${getOtherChannel(channel)}{display: none;}`);
+    });
+}
+
+function initialize() {
+    initializeNavigation() //
+        .then(() => waitForAllImages()) //
+        .then(makeSleep(300))
+        .then(() => window.executeCommand("show"));
+
+    document.querySelector("#minimize-button").onclick = () => {
+        window.executeCommand("minimize");
+    };
+
+    document.querySelector("#close-button").onclick = () => {
+        window.executeCommand("close");
+    };
+
+    adjustChannelElements();
+}
+
+window.showSettings = function() {
+    document.querySelector("#navigation>#settings.element").click();
+}
+
+function initializeNavigation() {
+    var elements = document.querySelectorAll("#navigation>.element");
+    elements.forEach(e => {
+        e.addEventListener("click", handleNavigationClick);
+    });
+
+    return loadInitialPage();
+}
+
+function removeActiveElement() {
+    var element = document.querySelector("#navigation>.element.active");
+    if (element) {
+        element.classList.remove("active");
+    }
+}
+
+function handleNavigationClick(e) {
+    const el = this;
+    if (el.classList.contains("active")) {
+        return;
+    }
+
+    removeActiveElement();
+    el.classList.add("active");
+    loadNavigationPage(el.id);
+}
+
+function loadInitialPage() {
+    const el = document.querySelector("#navigation>.element.active");
+    return loadNavigationPage(el.id);
+}
+
+function setInnerHTML(elm, html) {
+    elm.innerHTML = html;
+    Array.from(elm.querySelectorAll("script")).forEach(oldScript => {
+        const newScript = document.createElement("script");
+        Array.from(oldScript.attributes)
+            .forEach(attr => newScript.setAttribute(attr.name, attr.value));
+        newScript.appendChild(document.createTextNode(oldScript.innerHTML));
+        oldScript.parentNode.replaceChild(newScript, oldScript);
+    });
+}
+
+function loadNavigationPage(page) {
+    var content = document.querySelector("#content");
+    return fetch(`./pages/${page}.html`).then(data => {
+        return data.text()
+    }).then(text => {
+        setInnerHTML(content, text);
+    });
+}
